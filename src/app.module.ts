b3b37ca0121d4931f969dd86d7648f1e1e312e8b
maria29852164 <mariaramireeez2002@gmail.com>
2025-10-28 12:48:05 -0400

    feat: imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,15 @@ import { UserRepositoryDrizzle } from "./infrastructure/repositories/user.reposi
 import { PlanRepositoryDrizzle } from "./infrastructure/repositories/plan.repository.drizzle";
 import { SubscriptionRepositoryDrizzle } from "./infrastructure/repositories/subscription.repository.drizzle";
 import { FirestoreLogger } from "./infrastructure/firebase/firestore-logger";
-import { JwtModule } from "@nestjs/jwt";
+import {JwtModule, JwtService} from "@nestjs/jwt";
 import {RegisterUserUseCase} from "./application/use-cases/register-user.usecase";
 import {LoginUserUseCase} from "./application/use-cases/login.usecase";
 import {ListPlansUseCase} from "./application/use-cases/list-plans.usecase";
 import {SubscribeToPlanUseCase} from "./application/use-cases/subscribe.usecase";
 import {GetSubscriptionStatusUseCase} from "./application/use-cases/get-subscription-status.usecase";
+import {IUserRepository} from "./application/ports/user-repository.port";
+import {ISubscriptionRepository} from "./application/ports/subscription-repository.port";
+import {IPlanRepository} from "./application/ports/plan-repository.port";
 
 @Module({
     imports: [
@@ -33,16 +36,55 @@ import {GetSubscriptionStatusUseCase} from "./application/use-cases/get-subscrip
         LoginUserUseCase,
         SubscribeToPlanUseCase,
         GetSubscriptionStatusUseCase,
-
+       // PLANS
         {
             provide: "IPlanRepository",
             useClass: PlanRepositoryDrizzle,
         },
         {
             provide: ListPlansUseCase,
-            useFactory: (planRepo: any) => new ListPlansUseCase(planRepo),
+            useFactory: (planRepo: IPlanRepository) => new ListPlansUseCase(planRepo),
             inject: ["IPlanRepository"],
         },
+        // SUBS
+        {
+            provide:'ISubscriptionRepository',
+            useClass: SubscriptionRepositoryDrizzle
+        },
+        {
+            provide: GetSubscriptionStatusUseCase,
+            useFactory: (subRepo: ISubscriptionRepository) => new GetSubscriptionStatusUseCase(subRepo),
+        },
+        {
+            provide: SubscribeToPlanUseCase,
+            useFactory: (
+                subscriptionRepo: ISubscriptionRepository,
+                planRepo: IPlanRepository,
+                logger: FirestoreLogger
+            ) => new SubscribeToPlanUseCase(subscriptionRepo, planRepo, logger),
+            inject: [
+                "ISubscriptionRepository",
+                "IPlanRepository",
+                FirestoreLogger
+            ],
+        },
+        // USER
+        {
+            provide:'IUserRepository',
+            useClass: UserRepositoryDrizzle
+        },
+        {
+            provide: RegisterUserUseCase,
+            useFactory: (userRepo: IUserRepository, logger: FirestoreLogger) =>
+                new RegisterUserUseCase(userRepo, logger),
+            inject: ["IUserRepository", FirestoreLogger],
+        },
+        {
+            provide: LoginUserUseCase,
+            useFactory: (userRepo: IUserRepository, jwtService:JwtService) =>
+                new LoginUserUseCase(userRepo, jwtService),
+            inject: ["IUserRepository", JwtService],
+        }
     ],
 })
 export class AppModule {}
